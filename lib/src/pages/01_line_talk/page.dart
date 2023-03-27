8c2fbf0101835e3feb81a93710792e2a85273424
haru <haruki6180@gmail.com>
2023-03-27 21:05:32 +0900

    appbar作成

--- lib/src/pages/01_line_talk/page.dart
+++ lib/src/pages/01_line_talk/page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 
 class LineTalkPage extends StatelessWidget {
   static const text = "line";
+  static const allTalkCount = 99;
+  static const talkName = "太郎";
 
   const LineTalkPage({super.key});
 
@@ -11,23 +13,89 @@ class LineTalkPage extends StatelessWidget {
       appBar: PreferredSize(
           preferredSize: const Size.fromHeight(60),
           child: AppBar(
-              leading: Row(
-            children: [
-              IconButton(
-                onPressed: () => {},
-                icon: const Icon(
-                  Icons.arrow_back_ios_new,
+            elevation: 0,
+            backgroundColor: const Color.fromARGB(255, 99, 166, 217),
+            leadingWidth: 200,
+            leading: Row(
+              children: [
+                IconButton(
+                  color: Colors.black,
+                  onPressed: () => {},
+                  icon: const Icon(
+                    Icons.arrow_back_ios_new,
+                  ),
                 ),
-              ),
-              const Text("12"),
+                // ignore: prefer_const_constructors
+                Row(
+                  children: [
+                    Container(
+                      padding: const EdgeInsets.only(
+                        right: 10,
+                      ),
+                      child: Text(
+                        allTalkCount >= 99
+                            ? "$allTalkCount+"
+                            : allTalkCount.toString(),
+                        style: const TextStyle(
+                          fontSize: 20,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    const Text(
+                      talkName,
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    )
+                  ],
+                ),
+              ],
+            ),
+            actions: [
+              Row(
+                children: [
+                  IconButton(
+                    color: Colors.black,
+                    onPressed: () => {},
+                    icon: const Icon(
+                      Icons.search,
+                    ),
+                  ),
+                  IconButton(
+                    color: Colors.black,
+                    onPressed: () => {},
+                    icon: const Icon(
+                      Icons.call,
+                    ),
+                  ),
+                  IconButton(
+                    color: Colors.black,
+                    onPressed: () => {},
+                    icon: const Icon(
+                      Icons.menu,
+                    ),
+                  ),
+                ],
+              )
             ],
-          ))),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: const <Widget>[
-            Text(text),
-          ],
+          )),
+      body: Container(
+        decoration: const BoxDecoration(
+            image: DecorationImage(
+          image: AssetImage('images/line_talk_background.png'),
+          fit: BoxFit.cover,
+        )),
+        child: const Center(
+          child: Text(
+            'line',
+            style: TextStyle(
+              color: Colors.white,
+              fontSize: 30,
+            ),
+          ),
         ),
       ),
     );
