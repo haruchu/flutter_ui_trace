de7a33101579ff1d9ccc7f95e3dfb805d5dd0fb4
haru <haruki6180@gmail.com>
2023-03-30 04:40:33 +0900

    title, leadingの部分修正

--- lib/src/pages/01_line_talk/page.dart
+++ lib/src/pages/01_line_talk/page.dart
@@ -43,76 +43,64 @@ class LineTalkPage extends StatelessWidget {
               ),
     ];
     return Scaffold(
-      appBar: PreferredSize(
-          preferredSize: const Size.fromHeight(60),
-          child: AppBar(
-              elevation: 0,
-              backgroundColor: const Color.fromARGB(255, 99, 166, 217),
-              leadingWidth: 200,
-              leading: Row(
-                children: [
-                  IconButton(
-                    color: Colors.black,
-                    onPressed: () => {},
-                    icon: const Icon(
-                      Icons.arrow_back_ios_new,
-                    ),
-                  ),
-                  // ignore: prefer_const_constructors
-                  Row(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.only(
-                          right: 10,
-                        ),
-                        child: Text(
-                          allTalkCount >= 99
-                              ? "$allTalkCount+"
-                              : allTalkCount.toString(),
-                          style: const TextStyle(
-                            fontSize: 20,
-                            color: Colors.black,
-                          ),
-                        ),
-                      ),
-                      const Text(
-                        talkName,
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.black,
-                        ),
-                      )
-                    ],
-                  ),
-                ],
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: const Color.fromARGB(255, 99, 166, 217),
+        leading: IconButton(
+          color: Colors.black,
+          onPressed: () => {},
+          icon: const Icon(
+            Icons.arrow_back_ios_new,
+          ),
+        ),
+        title: Row(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(
+                right: 10,
               ),
-              title: Row(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  IconButton(
-                    color: Colors.black,
-                    onPressed: () => {},
-                    icon: const Icon(
-                      Icons.search,
-                    ),
-                  ),
-                  IconButton(
-                    color: Colors.black,
-                    onPressed: () => {},
-                    icon: const Icon(
-                      Icons.call,
-                    ),
-                  ),
-                  IconButton(
-                    color: Colors.black,
-                    onPressed: () => {},
-                    icon: const Icon(
-                      Icons.menu,
-                    ),
-                  ),
-                ],
-              ))),
+              child: Text(
+                allTalkCount >= 99 ? "$allTalkCount+" : allTalkCount.toString(),
+                style: const TextStyle(
+                  fontSize: 20,
+                  color: Colors.black,
+                ),
+              ),
+            ),
+            const Text(
+              talkName,
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+                color: Colors.black,
+              ),
+            )
+          ],
+        ),
+        actions: [
+          IconButton(
+            color: Colors.black,
+            onPressed: () => {},
+            icon: const Icon(
+              Icons.search,
+            ),
+          ),
+          IconButton(
+            color: Colors.black,
+            onPressed: () => {},
+            icon: const Icon(
+              Icons.call,
+            ),
+          ),
+          IconButton(
+            color: Colors.black,
+            onPressed: () => {},
+            icon: const Icon(
+              Icons.menu,
+            ),
+          ),
+        ],
+      ),
       body: Container(
           decoration: const BoxDecoration(
               image: DecorationImage(
